b5bcc42a41e0da27feca7a37a3052b82c4777015
Patrick Clements <patrick.clements@gmail.com>
2015-10-20 23:12:30 -0400

    add missing models to admin for visibility in GUI

--- wishlist_app/admin.py
+++ wishlist_app/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Item, WishlistGroup, GroupMember, Role
+from .models import Item, WishlistGroup, GroupMember, Role, Invite, RegistryAssignment, SecretSantaAssignment
 
 
 admin.site.register(Item)
 admin.site.register(WishlistGroup)
 admin.site.register(GroupMember)
 admin.site.register(Role)
+admin.site.register(Invite)
+admin.site.register(RegistryAssignment)
+admin.site.register(SecretSantaAssignment)
