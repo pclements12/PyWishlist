c51c647c4d53407e62ddbe1e18a24fc306305b57
Patrick Clements <patrick.clements@gmail.com>
2015-10-22 20:36:46 -0400

    show login errors, on registration from invite, go straight to group home

--- wishlist_app/views/operations.py
+++ wishlist_app/views/operations.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
-from wishlist_app.models import WishlistGroup
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_GET, require_http_methods
@@ -11,17 +9,18 @@ from django.views.decorators.http import require_GET, require_http_methods
 def request_login(request):
     if request.POST:
         print "attempting login"
+        form = AuthenticationForm(data=request.POST)
+        if not form.is_valid():
+            print "auth form is not valid"
+            form.add_error(None, "Login failed")
+            return render(request, "wishlist_app/login.html", {"login_form": form})
+        "auth form is valid"
         username = request.POST['username']
         password = request.POST['password']
         print "authenticating %s" % username
         user = authenticate(username=username, password=password)
-        if user is not None:
-            print "processing logging in"
-            login(request, user)
-            return redirect("wishlists")
-        else:
-            print "couldn't authenticate"
-            return HttpResponse('Unauthorized', status=401)
+        login(request, user)
+        return redirect("wishlists")
     else:
         form = AuthenticationForm()
         return render(request, "wishlist_app/login.html", {"login_form": form})
